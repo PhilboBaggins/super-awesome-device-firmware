2142a6291a1c42965f32445b2260f60304f648ce
Phil Baldwin <philip.baldwin@allieddata.com.au>
2018-04-12 17:46:57 +1000

    Basic LED blinking pattern

--- src/main.ino
+++ src/main.ino
@@ -1,10 +1,19 @@
 
+const int ONBOARD_LED_PIN = LED_BUILTIN;
+const int OFF_BOARD_LED_PIN = 1;
+
 void setup()
 {
-
+    pinMode(ONBOARD_LED_PIN, OUTPUT);
+    pinMode(OFF_BOARD_LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-
+    digitalWrite(ONBOARD_LED_PIN, HIGH);
+    digitalWrite(OFF_BOARD_LED_PIN, LOW);
+    delay(500);
+    digitalWrite(ONBOARD_LED_PIN, LOW);
+    digitalWrite(OFF_BOARD_LED_PIN, HIGH);
+    delay(500);
 }
